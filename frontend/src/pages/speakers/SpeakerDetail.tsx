afb5ca29c3a4487d4509172ba78162b6f2074989
Jakob Sack <mail@jakobsack.de>
2025-02-15 13:18:34 +0100

    Improve Speakers page

--- frontend/src/pages/speakers/SpeakerDetail.tsx
+++ frontend/src/pages/speakers/SpeakerDetail.tsx
@@ -1,83 +1,110 @@
 import { useState } from "react";
-import { useFetcher } from "react-router-dom";
+import { Link, useFetcher } from "react-router-dom";
 import type { Speaker } from "../../definitions";
+import Markdown from "react-markdown";
 
 interface SpeakerDetailParams {
   speaker: Speaker;
 }
 
+export type ViewMode = "inline" | "expand" | "form";
+
 export const SpeakerDetailComponent = ({ speaker }: SpeakerDetailParams) => {
-  const [showEdit, setShowEdit] = useState(false);
+  const [mode, setMode] = useState<ViewMode>("inline");
+
+  const toggleExpand = () => {
+    setMode(mode === "expand" ? "inline" : "expand");
+  };
 
   const toggleShowEdit = () => {
-    setShowEdit(!showEdit);
+    setMode(mode === "form" ? "expand" : "form");
   };
 
   const fetcher = useFetcher();
 
-  if (fetcher.formData && showEdit) {
-    setShowEdit(false);
+  if (fetcher.formData && mode === "form") {
+    setMode("expand");
     speaker.name = `${fetcher.formData.get("name")}`;
     speaker.description = `${fetcher.formData.get("description")}`;
   }
 
-  return (
-    <>
-      {showEdit ? (
-        <fetcher.Form method="post" action={`${speaker.id}/edit`}>
-          <div className="flex flex-col">
-            <div className="flex flex-row">
-              <div className="w-40">
-                <label htmlFor="formName">Name</label>
-              </div>
-              <div className="flex-1">
-                <input
-                  type="text"
-                  name="name"
-                  id="formName"
-                  className="border border-gray-300"
-                  placeholder="Name"
-                  defaultValue={speaker.name}
-                />
-              </div>
-            </div>
-            <div className="flex flex-row pt-2">
-              <div className="w-40">
-                <label htmlFor="formDescription">Description</label>
-              </div>
-              <textarea
-                className="border border-gray-300"
-                name="description"
-                id="formDescription"
-                placeholder="Description (optional)"
-                defaultValue={speaker.description}
-              />
-            </div>
-            <div className="flex flex-row pt-2">
-              <button type="submit" className="btn bg-primary-500 text-white p-1">
-                Update
-              </button>
-              <button
-                type="button"
-                className="btn bg-slate-500 text-white p-1 ml-3"
-                onClick={toggleShowEdit}
-                onKeyDown={toggleShowEdit}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </fetcher.Form>
-      ) : (
-        <>
-          <p>
-            {speaker.name} - {speaker.description || " "}
-          </p>
-          <p onClick={toggleShowEdit} onKeyDown={toggleShowEdit}>
+  if (mode === "inline") {
+    return (
+      <div className="flex flex-row w-full">
+        <div className="w-40">{speaker.name}</div>
+        <div className="flex-1 truncate">{(speaker.description || "").split("\n")[0]}</div>
+        <div className="w-20">
+          <button type="button" onClick={toggleExpand} onKeyDown={toggleExpand} className="btn variant-ghost p-1">
+            Expand
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  if (mode === "expand") {
+    return (
+      <div className="flex flex-row w-full">
+        <div className="w-40">{speaker.name}</div>
+        <div className="flex-1">
+          <Markdown>{speaker.description}</Markdown>
+        </div>
+        <div className="w-20">
+          <button type="button" onClick={toggleShowEdit} onKeyDown={toggleShowEdit} className="btn variant-ghost p-1">
             Edit
-          </p>
-        </>
-      )}
-    </>
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <fetcher.Form method="post" action={`${speaker.id}/edit`} className=" w-full">
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          <div className="w-40">
+            <label htmlFor="formName">Name</label>
+          </div>
+          <div className="flex-1">
+            <input
+              type="text"
+              name="name"
+              id="formName"
+              className="input variant-outlined border border-gray-300 p-1"
+              placeholder="Name"
+              defaultValue={speaker.name}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row pt-2">
+          <div className="w-40">
+            <label htmlFor="formDescription">Description</label>
+          </div>
+          <div className="flex-1">
+            <textarea
+              className="textarea variant-outlined border border-gray-300 p-1 h-48"
+              name="description"
+              id="formDescription"
+              placeholder="Description (optional)"
+              defaultValue={speaker.description}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row pt-2">
+          <div className="w-40" />
+          <button type="submit" className="btn variant-primary p-1">
+            Update
+          </button>
+          <button
+            type="button"
+            className="btn variant-soft p-1 ml-5"
+            onClick={toggleShowEdit}
+            onKeyDown={toggleShowEdit}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </fetcher.Form>
   );
 };
--- frontend/src/pages/speakers/SpeakersPage.tsx
+++ frontend/src/pages/speakers/SpeakersPage.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Speaker } from "../../definitions";
 
-import { Form, useLoaderData } from "react-router-dom";
+import { Form, useActionData, useLoaderData } from "react-router-dom";
 import type { ActionFunction, ActionFunctionArgs, LoaderFunction, LoaderFunctionArgs } from "react-router-dom";
 import { SpeakerDetailComponent } from "./SpeakerDetail";
 import { jwtFetch } from "../../common/jwtFetch";
@@ -27,23 +27,47 @@ export const speakersAction = (async (event: ActionFunctionArgs) => {
 }) satisfies ActionFunction;
 
 export const SpeakersComponent = () => {
-  const { speakers } = useLoaderData() as { speakers: Speaker[] };
+  const { speakers } = useLoaderData() as { speakers: Speaker[] | undefined };
+  const actionData = useActionData();
   const [showNew, setShowNew] = useState(!speakers || !speakers.length);
+
+  if (!speakers) {
+    return (
+      <section className="relative">
+        <div className="relative">
+          <div className="mx-auto px-6 max-w-7xl md:px-12">
+            <h1 className="podscribe">Speakers</h1>
+
+            <p>Loading</p>
+          </div>
+        </div>
+      </section>
+    );
+  }
 
   const toggleShowForm = () => {
     setShowNew(!showNew);
   };
 
+  useEffect(() => {
+    if (actionData) {
+      const form = document.getElementById("addSpeakerForm") as HTMLFormElement | null;
+      if (form) {
+        form.reset();
+      }
+    }
+  }, [actionData]);
+
   return (
     <section className="relative">
       <div className="relative">
         <div className="mx-auto px-6 max-w-7xl md:px-12">
-          <h1 className="text-3xl text-title font-semibold pb-2 underline">Speakers</h1>
+          <h1 className="podscribe">Speakers</h1>
 
-          <p>Here you can find all speakers in all episodes.</p>
-
-          {speakers ? (
+          {speakers.length ? (
             <>
+              <p>Here you can find all speakers in all episodes.</p>
+
               <ul className="mt-8 divide-y border-y *:py-3 *:flex *:items-center *:gap-3">
                 {speakers.map((x) => (
                   <li key={x.id}>
@@ -51,56 +75,71 @@ export const SpeakersComponent = () => {
                   </li>
                 ))}
               </ul>
-
-              {showNew ? (
-                <Form method="post">
-                  <div className="flex flex-col">
-                    <div className="flex flex-row">
-                      <div className="w-40">
-                        <label htmlFor="formName">Name</label>
-                      </div>
-                      <div className="flex-1">
-                        <input
-                          type="text"
-                          name="name"
-                          id="formName"
-                          className="border border-gray-300"
-                          placeholder="Name"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex flex-row pt-2">
-                      <div className="w-40">
-                        <label htmlFor="formDescription">Description</label>
-                      </div>
-                      <textarea
-                        className="border border-gray-300"
-                        name="description"
-                        id="formDescription"
-                        placeholder="Description (optional)"
-                      />
-                    </div>
-                    <div>
-                      <button type="submit" className="btn bg-primary-500 text-white p-1">
-                        Create Speaker
-                      </button>
-                      <p className="text-gray-400 text-sm pt-1">
-                        The episodes are assigned to the speaker at a later point in time using the Episode view.
-                      </p>
-                    </div>
-                  </div>
-                  <p onClick={toggleShowForm} onKeyDown={toggleShowForm}>
-                    Hide form
-                  </p>
-                </Form>
-              ) : (
-                <p onClick={toggleShowForm} onKeyDown={toggleShowForm}>
-                  Show new form
-                </p>
-              )}
             </>
           ) : (
-            <p>Loading</p>
+            <p>No speakers have been added yet.</p>
+          )}
+
+          {showNew ? (
+            <Form method="post" id="addSpeakerForm">
+              <h2 className="podscribe">Add Speaker</h2>
+              <div className="flex flex-col">
+                <div className="flex flex-row">
+                  <div className="w-40">
+                    <label htmlFor="formName">Name</label>
+                  </div>
+                  <div className="flex-1">
+                    <input
+                      type="text"
+                      name="name"
+                      id="formName"
+                      className="input variant-outlined border border-gray-300 p-1"
+                      placeholder="Name"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-row pt-2">
+                  <div className="w-40">
+                    <label htmlFor="formDescription">Description</label>
+                  </div>
+                  <div className="flex-1">
+                    <textarea
+                      className="textarea variant-outlined border border-gray-300 p-1 h-48"
+                      name="description"
+                      id="formDescription"
+                      placeholder="Description (optional)"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-row pt-2">
+                  <div className="w-40" />
+                  <button type="submit" className="btn variant-primary p-1">
+                    Create Speaker
+                  </button>
+                  {speakers.length ? (
+                    <button
+                      type="button"
+                      className="btn variant-soft p-1 ml-5"
+                      onClick={toggleShowForm}
+                      onKeyDown={toggleShowForm}
+                    >
+                      Cancel
+                    </button>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+            </Form>
+          ) : (
+            <button
+              type="button"
+              className="btn variant-ghost mt-8 p-1"
+              onClick={toggleShowForm}
+              onKeyDown={toggleShowForm}
+            >
+              Add speaker
+            </button>
           )}
         </div>
       </div>
